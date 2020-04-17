4456431c17a5c341dc4c09a2a65517a012b4822e
otreblan <otreblain@gmail.com>
2020-04-17 12:49:43 -0500

    Now server.py is __init__.py

--- web/__init__.py
+++ web/__init__.py
@@ -50,6 +50,10 @@ def esprimo(n: int) -> str:
     return 'True'
 
 
-if __name__ == '__main__':
+def main():
     app.secret_key = ".."
     app.run(port=8080, threaded=True, host=('127.0.0.1'))
+
+
+if __name__ == '__main__':
+    main()
